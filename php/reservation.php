3c26c0dbe682a1d88afbb034945f0280948d3e4f
MarcCarbonnier <marc.carbonnier@hotmail.com>
2024-09-12 11:11:09 +0200

    *news

---
+++ php/reservation.php
@@ -0,0 +1,25 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD']== 'POST'){
+    $name = htmlspecialchars($_POST['nom']);
+    $couvert = (int)$_POST['guests'];
+    $date = htmlspecialchars($_POST['date']);
+    $time = htmlspecialchars($_POST['time']);
+
+    if(empty($name) || $couvert <= 0 || empty($date) || empty($time)){}
+        else {
+            echo "<h2>Merci de votre réservation, $name ! </h2>";
+            echo "<p>Détails de votre réservation :</p>";
+            echo "<ul>";
+            echo "<li>Nom : $name</li>";
+            echo "<li>Date : $date</li>";
+            echo "<li>Heure : $time</li>";
+            echo "<li>Nombre de personnes : $couvert</li>";
+            echo "</ul>";
+        }
+}
+else {
+    header('Location: ../reserver.html');
+}
+
+?>
